38a17bdb25ac291d1c74b2a8b9d8ad6deb912f3c
fabio <cappiello.fabio@gmail.com>
2023-01-05 08:28:43 +0000

    05-01-2022

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -2,8 +2,8 @@ export const msalConfig = {
   auth: {
     clientId: "8006c866-1a41-432a-8d1f-690b7924b126",
     authority: "https://login.microsoftonline.com/35bd1325-af2b-43f0-a106-3efdcfed0b07",
-    redirectUri: "https://witty-sand-0686f5710.2.azurestaticapps.net/",
-    //redirectUri: "http://localhost:3000",
+    //redirectUri: "https://witty-sand-0686f5710.2.azurestaticapps.net/",
+    redirectUri: "http://localhost:3000",
   },
   cache: {
     cacheLocation: "sessionStorage",
@@ -24,7 +24,7 @@ export const loginRequest = {
 };
 
 export const apiRequestGetAllUsers = {
-  url: "https://hello-auth-world.azurewebsites.net/api/GetAllUsers??code=ovIfMxPE7VjiJSGhcx3Hu_yGa8VgrlkCyTkgiPCiBxuiAzFuZeNIRg==",
+  url: "https://hello-auth-world.azurewebsites.net/api/GetAllUsers",
   //url: "https://hello-auth-world.azurewebsites.net/api/Hello",
   scopes: ["api://8006c866-1a41-432a-8d1f-690b7924b126/user_impersonation"],
 };
